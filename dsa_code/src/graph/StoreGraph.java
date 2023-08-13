22b7321d7e413374029664f1a34329dd8f3869b7
Mohd Sufiyan <sufiyan@timelybills.app>
2023-08-13 23:37:25 +0530

    added comments

--- dsa_code/src/graph/StoreGraph.java
+++ dsa_code/src/graph/StoreGraph.java
@@ -9,59 +9,99 @@ public class StoreGraph {
 	private int adjMatrix[][];
 	private ArrayList<ArrayList<Integer>> adjList;
 	private boolean isMatrix;
-	
-	
+
+	/**
+	 * construct takes vertices and edges and construct adjacency matrix and
+	 * adjacency list
+	 * 
+	 * @param vertices
+	 * @param edges
+	 */
 	// by default store 1 based graph
-	StoreGraph(int vertices, int edges){
+	StoreGraph(int vertices, int edges) {
 		this.vertices = vertices;
 		this.edges = edges;
 		this.isMatrix = true;
-		adjMatrix = new int[vertices+1][vertices+1];
+		adjMatrix = new int[vertices + 1][vertices + 1];
 		adjList = new ArrayList<>();
-		for(int i=0; i<vertices+1; ++i) {
+		for (int i = 0; i < vertices + 1; ++i) {
 			adjList.add(new ArrayList<>());
 		}
 	}
-	
-	StoreGraph(int vertices, int edges, boolean isMatrix, boolean isZeroBased){
+
+	/**
+	 * construct creates list or matrix on the preference of arguments
+	 * 
+	 * @param vertices
+	 * @param edges
+	 * @param isMatrix
+	 * @param isZeroBased
+	 */
+	StoreGraph(int vertices, int edges, boolean isMatrix, boolean isZeroBased) {
 		this.vertices = vertices;
 		this.edges = edges;
 		this.isMatrix = isMatrix;
 		int size = isZeroBased ? vertices : (vertices + 1);
-		if(!isMatrix) {
+		if (!isMatrix) {
 			adjList = new ArrayList<>();
-			for(int i=0; i<size; ++i) {
+			for (int i = 0; i < size; ++i) {
 				adjList.add(new ArrayList<>());
 			}
-		}else {
+		} else {
 			adjMatrix = new int[size][size];
-			for(int i=0; i<size; ++i) {
+			for (int i = 0; i < size; ++i) {
 				Arrays.fill(adjMatrix[i], 0);
 			}
 		}
 	}
-	
+
+	/**
+	 * getter for vertices
+	 * 
+	 * @return
+	 */
 	public int getVertices() {
 		return vertices;
 	}
-	
+
+	/**
+	 * getter for edges
+	 * 
+	 * @return
+	 */
 	public int getEdges() {
 		return edges;
 	}
-	
-	public int[][] getMatrix(){
+
+	/**
+	 * getter for adjacency matrix
+	 * 
+	 * @return
+	 */
+	public int[][] getMatrix() {
 		return adjMatrix;
 	}
-	
-	public ArrayList<ArrayList<Integer>> getList(){
+
+	/**
+	 * getter for adjacency list
+	 * 
+	 * @return
+	 */
+	public ArrayList<ArrayList<Integer>> getList() {
 		return adjList;
 	}
-	
+
+	/**
+	 * this method adds node to list or matrix
+	 * 
+	 * @param u
+	 * @param v
+	 */
 	public void addNode(int u, int v) {
-		if(!isMatrix) {
+		if (!isMatrix) {
 			adjList.get(u).add(v);
 			adjList.get(v).add(u);
-		}else {
+		} else {
 			adjMatrix[u][v] = 1;
 			adjMatrix[v][u] = 1;
 		}
